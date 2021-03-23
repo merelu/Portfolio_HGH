e03b879ce80b60a999330a98b496e6c41595545b
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-23 18:08:45 +0900

    react 초기셋팅완(router, @loadable_component, @emotion)

--- front/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return <div className="App">{"hello"}</div>;
-}
-
-export default App;
