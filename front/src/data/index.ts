c6067ddb920cdf26b76bf5a288c2ad56d552e032
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-28 20:58:25 +0900

    더미 data 추가

---
+++ front/src/data/index.ts
@@ -0,0 +1,72 @@
+import mainImg from "../images/main.svg";
+import secondImg from "../images/second.jpg";
+import thirdImg from "../images/third.svg";
+import fourthImg from "../images/fourth.svg";
+
+export const homeObjfirst = {
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  topLine: " Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  headline: "Lorem ipsum dolor sit amet",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel: "Hire me",
+  start: "",
+  img: mainImg,
+  alt: "Image",
+};
+
+export const homeObjSecond = {
+  primary: false,
+  lightBg: true,
+  imgStart: "start",
+  lightTopLine: false,
+  lightTextDesc: false,
+  lightText: false,
+  topLine: " Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  headline: "Lorem ipsum dolor sit amet",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel: "Hire me",
+  start: "true",
+  img: secondImg,
+  alt: "Image",
+};
+
+export const homeObjThird = {
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  topLine: " Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  headline: "Lorem ipsum dolor sit amet",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel: "Hire me",
+  start: "",
+  img: thirdImg,
+  alt: "Image",
+};
+
+export const homeObjFourth = {
+  primary: true,
+  lightBg: false,
+  imgStart: "start",
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  topLine: " Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+  headline: "Lorem ipsum dolor sit amet",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  buttonLabel: "Hire me",
+  start: "true",
+  img: fourthImg,
+  alt: "Image",
+};
