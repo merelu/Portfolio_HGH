18f76b211900dd3a59617b652a2266c9b00c711c
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-28 21:01:16 +0900

    Home page 추가

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import InfoSection from "@components/InfoSection";
+import {
+  homeObjfirst,
+  homeObjSecond,
+  homeObjThird,
+  homeObjFourth,
+} from "../../data";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <InfoSection {...homeObjfirst} />
+      <InfoSection {...homeObjSecond} />
+      <InfoSection {...homeObjThird} />
+      <InfoSection {...homeObjFourth} />
+    </>
+  );
 }
 
 export default Home;
