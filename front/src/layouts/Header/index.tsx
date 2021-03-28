9ba18b8c1a21a5cd88ceb1481983b42cd65914cb
GYUHA_HWANG <gyuha0310@gmail.com>
2021-03-28 20:59:18 +0900

    안쓰는 파일삭제

--- front/src/layouts/Header/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-function Header() {
-  return <AppBar></AppBar>;
-}
-
-export default Header;
--- front/src/pages/Services/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Services() {
-  return <div>services</div>;
-}
-
-export default Services;
